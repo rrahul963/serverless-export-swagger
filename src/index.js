19d04db9c266cf9412ec3dd8d7491c20503597f5
Tulsian, Rahul <Rahul.Tulsian@nordstrom.com>
2019-01-10 11:20:36 -0800

    updated code to use async await

--- src/index.js
+++ src/index.js
@@ -1,77 +1,75 @@
-/**
- * The export swagger function.
- * This function will:
- * 1. get the stack outputs.
- * 2. look for api gateway name.
- * 3. call getExport endpoint to get the swagger.
- * 4. upload the swagger to specified S3 bucket.
- *
- * @param      {Object}  serverless  The serverless
- */
-const exportApi = function exportApi(serverless) {
-  let apiName = ''
-  serverless.getProvider('aws').request(
-    'CloudFormation',
-    'describeStacks',
-    {
-      StackName: `${serverless.service.getServiceName()}-${serverless.getProvider('aws').getStage()}`,
-    },
-    serverless.getProvider('aws').getStage(),
-    serverless.getProvider('aws').getRegion(),
-  ).then((result) => {
-    if (result) {
-      const outputs = result.Stacks[0].Outputs
-      outputs.forEach((output) => {
-        if (output.OutputKey === 'ServiceEndpoint') {
-          const [httpApiName] = output.OutputValue.split('.');
-          [, apiName] = httpApiName.split('//')
-        }
-      })
-      serverless.getProvider('aws').request(
-        'APIGateway',
-        'getExport',
-        {
-          exportType: 'swagger',
-          restApiId: apiName,
-          stageName: serverless.getProvider('aws').getStage(),
-          accepts: 'application/json',
-          parameters: {
-            extensions: 'integrations',
-          },
-        },
-        serverless.getProvider('aws').getStage(),
-        serverless.getProvider('aws').getRegion(),
-      ).then((r) => {
-        if (r) {
-          const settings = serverless.service.custom
-          if ('swaggerDestinations' in settings) {
-            if (
-              's3BucketName' in settings.swaggerDestinations
-              && 's3KeyName' in settings.swaggerDestinations
-            ) {
-              serverless.getProvider('aws').request(
-                'S3',
-                'putObject',
-                {
-                  Body: r.body,
-                  Bucket: settings.swaggerDestinations.s3BucketName,
-                  Key: settings.swaggerDestinations.s3KeyName,
-                },
-                serverless.getProvider('aws').getStage(),
-                serverless.getProvider('aws').getRegion(),
-              ).then((res) => {
-                if (res) {
-                  serverless.cli.log('File uploded to S3')
-                } else {
-                  serverless.cli.log('Failed to upload file to S3')
-                }
-              })
-            }
-          }
-        }
-      })
-    }
+const AWS = require('aws-sdk')
+
+const getApiName = async function getApiName(serviceName, stage, region, creds) {
+  const cfn = new AWS.CloudFormation({
+    credentials: creds,
+    region,
   })
+  let apiName = ''
+  const stack = await cfn.describeStacks({
+    StackName: `${serviceName}-${stage}`,
+  }).promise()
+  if (stack) {
+    const outputs = stack.Stacks[0].Outputs
+    outputs.forEach((output) => {
+      if (output.OutputKey === 'ServiceEndpoint') {
+        const [httpApiName] = output.OutputValue.split('.');
+        [, apiName] = httpApiName.split('//')
+      }
+    })
+  }
+  return apiName
+}
+
+const getSwagger = async function getSwagger(apiName, stage, region, creds) {
+  const ag = new AWS.APIGateway({
+    credentials: creds,
+    region,
+  })
+  const swagger = await ag.getExport({
+    exportType: 'swagger',
+    restApiId: apiName,
+    stageName: stage,
+    accepts: 'application/json',
+    parameters: {
+      extensions: 'integrations',
+    },
+  }).promise()
+  return swagger
+}
+
+const uploadSwaggerToS3 = async function uploadSwaggerToS3(swagger, bucket, key, region, creds) {
+  const s3 = new AWS.S3({
+    credentials: creds,
+    region,
+  })
+  await s3.putObject({
+    Body: swagger.body,
+    Bucket: bucket,
+    Key: key,
+  }).promise()
+}
+
+const getBucketAndKey = function getBucketAndKey(serverless) {
+  const bucket = serverless.service.custom.swaggerDestinations.s3BucketName
+  const key = serverless.service.custom.swaggerDestinations.s3KeyName
+  if (!bucket || !key) {
+    throw new Error('ExportSwagger: Bucket name and key are required fields')
+  }
+  return { bucket, key }
+};
+
+const exportApi = async function exportApi(serverless) {
+  const provider = serverless.getProvider('aws')
+  const awsCredentials = provider.getCredentials().credentials
+  const region = provider.getRegion()
+  const stage = provider.getStage()
+  const serviceName = serverless.service.getServiceName()
+  const { bucket, key } = getBucketAndKey(serverless)
+  const apiName = await getApiName(serviceName, stage, region, awsCredentials)
+  const swagger = await getSwagger(apiName, stage, region, awsCredentials)
+  await uploadSwaggerToS3(swagger, bucket, key, region, awsCredentials)
+  serverless.cli.consoleLog('ExportSwagger: File uploaded to s3.')
 }
 
 /**
@@ -82,8 +80,8 @@ class ExportSwagger {
     this.serverless = serverless
     this.options = options
     this.hooks = {
-      'after:deploy:deploy': function () {
-        exportApi(serverless)
+      'after:deploy:deploy': async function () {
+        await exportApi(serverless)
       },
     }
   }
